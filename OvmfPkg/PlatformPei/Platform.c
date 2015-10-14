12910bd170a4f8add22c526c9eabe33fa0840da4
Laszlo Ersek <lersek@redhat.com>
2015-10-14 15:59:06 +0200

    OvmfPkg: take PcdResizeXterm from the QEMU command line (RH only)
    Notes about the 20160608b-988715a -> 20170228-c325e41585e3 rebase:
    
    - no changes
    
    Notes about the 20170228-c325e41585e3 -> 20171011-92d07e48907f rebase:
    
    - refresh downstream-only commit 8abc2a6ddad2 against context differences
      in the DSC files from upstream commit 5e167d7e784c
      ("OvmfPkg/PlatformPei: don't allocate reserved mem varstore if
      SMM_REQUIRE", 2017-03-12).
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    (cherry picked from commit 6fa0c4d67c0bb8bde2ddd6db41c19eb0c40b2721)
    (cherry picked from commit 8abc2a6ddad25af7e88dc0cf57d55dfb75fbf92d)
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- OvmfPkg/PlatformPei/Platform.c
+++ OvmfPkg/PlatformPei/Platform.c
@@ -813,6 +813,7 @@ InitializePlatform (
     PeiFvInitialization ();
     MemMapInitialization ();
     NoexecDxeInitialization ();
+    UPDATE_BOOLEAN_PCD_FROM_FW_CFG (PcdResizeXterm);
   }
 
   InstallClearCacheCallback ();
